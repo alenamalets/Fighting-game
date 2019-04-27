2175fcf8ce5e47a582248ea8443993ee2d312492
Alena <elen.malets@gmail.com>
2019-04-27 11:29:48 +0200

    adding game functions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -6,7 +6,30 @@ const hero = {
     inventory: [],
     health: 10,
     weapon: {
-        type:'Sword',
+        type:'dagger',
         damage: 2
     }
 };
+
+function rest(heroObj) {
+    if (heroObj.health === 10) {
+        alert('Health property of that object already has the value 10!');
+    }
+    else {
+        heroObj.health = 10
+    }
+    return heroObj;
+}
+
+function pickUpItem(heroObj, weaponObj) {
+    heroObj.inventory.push(weaponObj);
+}
+
+function equipWeapon(heroObj) {
+    if(heroObj.inventory.length === 0) {
+        return null;
+    }
+    else {
+        heroObj.weapon = heroObj.inventory[0];
+    }
+}

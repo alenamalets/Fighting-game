3549b898cbf8917c40e59614eaf320b2d40384fd
Alena <elen.malets@gmail.com>
2019-04-27 13:39:15 +0200

    adding displayStats function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -33,3 +33,15 @@ function equipWeapon(heroObj) {
         heroObj.weapon = heroObj.inventory[0];
     }
 }
+
+function displayStats(heroObj) {
+    const name = `<b>Name:</b> ${heroObj.name}`;
+    const health = `<b>Health:</b> ${heroObj.health}`;
+    const type = `<b>Weapon type:</b> ${heroObj.weapon.type}`;
+    const damage = `<b>Weapon damage:</b> ${heroObj.weapon.damage}`;
+    const div = document.getElementById('hero-info');
+    div.innerHTML = `${name}<br>${health}<br>${type}<br>${damage}`;
+    
+}
+
+displayStats(hero);
